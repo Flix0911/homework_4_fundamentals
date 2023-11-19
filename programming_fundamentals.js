a2b2e1d478caaacccb471af952cb2b2c4a1a14f7
Erick Valencia <evalencia0911@gmail.com>
2023-11-19 11:33:59 -0700

    completed responses to terms

--- programming_fundamentals.js
+++ programming_fundamentals.js
@@ -3,3 +3,45 @@
 // ----
 
 //completed reading programming principles
+
+
+//Responses
+
+
+
+// DRY
+    //Don't Repeat Yourself. Don't do the same thing you just did somewhere in the program
+        //Use the tools/functionalities built in that are simplified and can be relied on later
+
+// KISS
+    //Keep it simple, stupid. Don't write something overly abstract or solving something in a convuluted manner
+        //write something that is to the point, legible to you and others, and that can be clearly understood what it is doing
+
+// Avoid creating a YAGNI
+    //You aren't going to need it. Don't create something that you want to add in the future. Create what is current
+        //and necessary to what you're going. Everything written should be used in the current moment/set up
+
+// Do the simplest thing that could possibly work
+    //Similar to KISS. Simplicity helps you and others who read your work, complex solutions are only adding trouble
+
+// Don't make me think
+    //Everything you write should be easy to comprehend by others, straining others to understand your code isn't beneficial at all
+
+// Write code for the maintainer
+    //Write with the mindset that someone else is going to maintain this, even if that someone is you, because legibleness and simplicity are easier
+
+// Single responsibility principle
+    //Everything written should be doing an excellent job at the one task it is assigned
+
+// Avoid premature optimization
+    //Ensure everything works 1st and is doing what you want, worry about efficiency after that
+
+// Separation of concerns
+    //Seperate what is accomplishing what into distinct sets/locations in the code.
+
+//Which one surprises you
+    //Optimization was an interesting one considering that is how everything thinks, we must always strive to more efficiency
+
+//which one is currently giving you the most struggle
+    //A lot of these. I'm repeating myself, not keeping it simple, most likely making teachers/graders think too hard, not everything is doing its job perfectly.
+    //I think it will come with time but alas
